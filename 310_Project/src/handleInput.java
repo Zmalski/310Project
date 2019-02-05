f6382c461481e7dba15caada31bd37f494a4b59f
rathore90 <rathore90@hotmail.com>
2019-02-05 12:57:45 -0800

    Determine Output Classes

--- 310_Project/src/handleInput.java
+++ 310_Project/src/handleInput.java
@@ -5,6 +5,9 @@ public class handleInput {
 	public handleInput(String name) {
 		this.name = name;
 	}
+	public handleInput() {
+		// TODO Auto-generated constructor stub
+	}
 	public String getName() {
 		return this.name;
 	}
