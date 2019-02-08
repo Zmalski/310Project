91f01aa5a96e8b91dca3c7c08a6a32ae317069e2
rathore90 <rathore90@hotmail.com>
2019-02-07 21:26:11 -0800

    I have done some changes in determineoutput class

--- 310_Project/src/determineOutput.java
+++ 310_Project/src/determineOutput.java
@@ -5,23 +5,22 @@ public class determineOutput {
 	 * Pardeep
 	 */
 	public static void main(String[] arg) {
-		Scanner reader = new Scanner(System.in);
-		System.out.println("Hello, nice to meet you! I'm BerthaBot, what's your name?");
-		String name = reader.nextLine(); 
-		System.out.println("Alright " + name + " lets start out blind date!");	
-		boolean exit = true;
-		while(exit) {
+	 Scanner reader = new Scanner(System.in);
+	 System.out.println("Hello, nice to meet you! I'm BerthaBot, what's your name?");
+	 String name = reader.nextLine(); 
+	 System.out.println("Alright " + name + " lets start out blind date!");	
+    /*
+     * While statement to keep the conversation continue or terminate the conversation.
+     * I have created a demo of static conversation. we need to implement the object oriented concepts though.
+     */
+	 boolean exit = true;
+	 while(exit) {
 		String bye = reader.nextLine();
 		if(bye.equals("bye")) {
 			exit = false;
 		}else {
 			System.out.println("What do you do for living " + name);
-		}
-			
-		
-		
-		
-		
+		}	
 		/*
 		handleInput inputHandler = new handleInput(name);
 		inputHandler.getName();
