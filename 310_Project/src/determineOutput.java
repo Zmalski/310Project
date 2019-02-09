81c13aeb637ae2c7a6fbd09784eb38f299cff50f
rathore90 <rathore90@hotmail.com>
2019-02-09 13:44:20 -0800

    Determine output according to the input.

--- 310_Project/src/determineOutput.java
+++ 310_Project/src/determineOutput.java
@@ -1,70 +1,90 @@
 import java.util.Scanner;
 
 public class determineOutput {
+	
+	
+	handleInput hInput = new handleInput();
 	/*
 	 * Pardeep
-	 */
-	public static void main(String[] arg) {
-	 Scanner reader = new Scanner(System.in);
-	 System.out.println("Hello, nice to meet you! I'm BerthaBot, what's your name?");
-	 String name = reader.nextLine(); 
-	 System.out.println("Alright " + name + " lets start out blind date!");	
-    /*
-     * While statement to keep the conversation continue or terminate the conversation.
-     * I have created a demo of static conversation. we need to implement the object oriented concepts though.
-     */
-	 boolean exit = true;
-	 String professionResponse = null;
-	 while(exit) {
-		String bye = reader.nextLine();
-		if(bye.equals("bye")) {
-			exit = false;
-		}else {
-			System.out.println("What do you do for living " + name + "?");
-			String profession = reader.nextLine();
-			int responseback = response(profession);
-			//Switch statement returns the output according to the response.
-		        switch (responseback) {
-		            case 1:  professionResponse = "Software Developer"; 
-		            		 System.out.println("Software developer. Wow! thats great.");
-		            		 exit = false;
-		                     break;
-		            case 2:  professionResponse = "Musician";
-		            		 System.out.println("Musician. I love music.");
-		                     exit = false;
-                    		 break;
-		            case 3:  professionResponse = "Sports Player";
-		            		 System.out.println("Player!!! Are you a soccer player. I love soccer");
-		            		 exit = false;
-		            		 break;
-		            default: professionResponse = "Invalid entry";
-		                     System.out.println("This input is not allowed. ");
-		                     exit = false;
-		                     break;
-		        }
-		    }	
-		/*
-		handleInput inputHandler = new handleInput(name);
-		inputHandler.getName();
-		inputHandler.getUserInput();	
-	*/
-		} 
-	 System.out.println("\n" + "Good to know that you are a " + professionResponse +  "\n" + "Sorry!! I gotta go. Got a call from work. I will talk to you soon ");
+		 */
+		String inputReceive = hInput.getUserInput();
+		String responseback = hInput.checkOccupation(inputReceive);
+		static String professionResponse = null;
+		
+		public static String responseBack(String responseback) {
+				// Switch statement returns the output according to the response.
+				switch (responseback) {
+				case "developer":
+					professionResponse = "Software Developer";
+					System.out.println("Software developer. Wow! thats great.");
+					break;
+				case "musician":
+					professionResponse = "Musician";
+					System.out.println("Musician. I love music.");
+					break;
+				case "player":
+					professionResponse = "Sports Player";
+					System.out.println("Player!!! Are you a soccer player. I love soccer");
+					break;
+				case "butcher":
+					professionResponse = "butcher";
+					System.out.println("Butcher!!! thats a good job.");
+					break;
+				case "cook":
+					professionResponse = "cook";
+					System.out.println("Cook!!! I love cooking.");
+					break;
+				case "farmer":
+					professionResponse = "farmer";
+					System.out.println("Farmer!!! I love fields");
+					break;
+				case "firefighter":
+					professionResponse = "firefighter";
+					System.out.println("Firefighter!!! that's cool.");
+					break;
+				case "hairdresser":
+					professionResponse = "hairdresser";
+					System.out.println("Hairdresser, wow!!! thats a cool job.");
+					break;
+				case "journalist":
+					professionResponse = "journalist";
+					System.out.println("Journalist!!! it must be a hectic job.");
+					break;
+				case "lawyer":
+					professionResponse = "lawyer";
+					System.out.println("Lawyer!!! great.");
+					break;
+				case "mechanic":
+					professionResponse = "mechanic";
+					System.out.println("Mechanic!!! great.");
+					break;
+				case "painter":
+					professionResponse = "painter";
+					System.out.println("Painter!!! You are an artist.");
+					break;
+
+				default:
+					professionResponse = "Invalid entry";
+					System.out.println("This input is not allowed. ");
+					break;
+				}
+			return professionResponse;
+		//System.out.println("\n" + "Good to know that you are a " + professionResponse + "\n"
+			//	+ "Sorry!! I gotta go. Got a call from work. I will talk to you soon ");
 	}
-	
-	/*
-	 * I have created a method that parse the whole string, and if in the given string by the user has the profession matches to our scope, 
-	 * the bot will response accordingly. 
-	 */
-	public static int response(String profession) {
-		String Str = new String(profession);
-		if(Str.matches("(.*)developer(.*)")) {
-			return 1;
-		}if(Str.matches("(.*)musician(.*)")) {
-			return 2;
-		}if(Str.matches("(.*)player(.*)")) {
-			return 3;	
-		}else 
-			return -1;
-	 }
 }
+	/*
+	 * I have created a method that parse the whole string, and if in the given
+	 * string by the user has the profession matches to our scope, the bot will
+	 * response accordingly. 
+	 * public static int response(String profession) { 
+	 * String Str = new String(profession); 
+	 * if(Str.matches("(.*)developer(.*)")) {
+	 *  return 1; }
+	 *  if(Str.matches("(.*)musician(.*)")) { 
+	 *  return 2;
+	 * }if(Str.matches("(.*)player(.*)")) { 
+	 * return 3; }
+	 * else return -1; 
+	 * }
+	 */
