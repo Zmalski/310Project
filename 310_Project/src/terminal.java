372ec2b0af1408e4c94ace7979fe824f191c1c3b
rathore90 <rathore90@hotmail.com>
2019-02-09 14:01:44 -0800

    Determine output is working according to the input given. check terminal class for testing.

--- 310_Project/src/terminal.java
+++ 310_Project/src/terminal.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 public class terminal {
 
 	public static void main(String[] args) {
-		Scanner reader = new Scanner(System.in);
 		handleInput inputHandler = new handleInput();
-		determineOutput outputDeterminer = new determineOutput();
+		//determineOutput outputDeterminer = new determineOutput();
+		
+		Scanner reader = new Scanner(System.in);
 		System.out.println("You are on a blind date. Would you like to date a man or a woman?");
 		while(true) {
 			String response = reader.nextLine();
 			String occ = inputHandler.checkOccupation(response);
-			System.out.println(occ);
+			String output = determineOutput.responseBack(occ);
+			output.toString();
 		}
-
 	}
 
 }
