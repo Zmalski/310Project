16860a6c5d86e78176bb3b8a8df507dbfa54c247
Zachary Maludzinski <Zacharymaludzinski@gmail.com>
2019-02-09 13:30:38 -0800

    Terminal is functional, occupation checking works

--- 310_Project/src/terminal.java
+++ 310_Project/src/terminal.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 
 public class terminal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);
+		handleInput inputHandler = new handleInput();
+		determineOutput outputDeterminer = new determineOutput();
+		System.out.println("You are on a blind date. Would you like to date a man or a woman?");
+		while(true) {
+			String response = reader.nextLine();
+			String occ = inputHandler.checkOccupation(response);
+			System.out.println(occ);
+		}
 
 	}
 
