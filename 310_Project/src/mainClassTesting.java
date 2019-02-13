d85ad5fa6f1fa6922afe9a1e26c34fe0656397c2
rathore90 <rathore90@hotmail.com>
2019-02-12 17:03:26 -0800

    main class for testing.

--- 310_Project/src/mainClassTesting.java
+++ 310_Project/src/mainClassTesting.java
@@ -4,21 +4,6 @@ public class mainClassTesting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<String> list = new ArrayList<String>();
-		list.add(0, "depp");
-		list.add(1, "dskjbfjkdbsf");
-		list.add(2, "jkdfj");
-		list.add(3, "kiohsfjbsd");
-		list.add(4, "gibe");
-		list.add(5, "five");
-		
-		String listString = "";
-			   
-			   for(String s : list) {
-				   listString += s + "\t";
-			   }
-		System.out.println(listString + "\t");	
 
-	}
 
 }
