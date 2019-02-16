6f86f030675f4a9096c4312103b8e219317279db
Zach <zacharymaludzinski@gmail.com>
2019-02-16 02:12:39 -0800

    Question asker randomness

--- 310_Project/src/questionAsker.java
+++ 310_Project/src/questionAsker.java
@@ -10,7 +10,8 @@ public class questionAsker {
 	public boolean sports = false;
 
 	/**
-	 * Returns an array containing a question to ask user and a keyword for data processing
+	 * Returns an array containing a question to ask user and a keyword for data
+	 * processing
 	 * 
 	 * @param void
 	 * @return Array containing question and question keyword
@@ -18,8 +19,15 @@ public class questionAsker {
 	public String[] ask() {
 		// TODO Add some randomization to the questions so there is variety.
 		String outputArray[] = new String[1];
+		int random = (int) (Math.random() * 2 + 1);
 		if (movies == false) {
-			outputArray[0] = "What movies do you like?";
+			//Picks one of three questions, randomly.
+			if (random == 1)
+				outputArray[0] = "What movies do you like?";
+			else if (random == 2)
+				outputArray[0] = "Seen any good movies lately?";
+			else
+				outputArray[0] = "What are some of your favourite movies?";
 			outputArray[1] = "movies";
 			movies = true;
 		} else if (music == false) {
@@ -42,18 +50,16 @@ public class questionAsker {
 			outputArray[0] = "What music do you like?";
 			outputArray[1] = "music";
 			music = true;
-		}
-		else {
+		} else {
 			outputArray[0] = "Sorry, I'm out of things to talk about!";
 			outputArray[1] = "invalid";
 		}
 		return outputArray;
 	}
-	
+
 	public String afterAsk(String input, String qdata) {
 		// TODO implement
-		
-		
+
 		return "";
 	}
 
