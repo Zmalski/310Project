dcb4d609817df205d04f4a0be6f46a8380e0b7cc
Eric <Eric@206.12.35.37>
2019-02-17 19:36:42 -0800

    my sad sad gui. i will make it more beautifuller

--- 310_Project/src/terminalwgui.java
+++ 310_Project/src/terminalwgui.java
@@ -1,3 +1,5 @@
+//import test.Stage;
+import java.util.Optional;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -8,6 +10,7 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
@@ -17,38 +20,31 @@ import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 public class terminalwgui extends Application{
-	public String username = "Human";
-	public String chatbotname = "CHATBOTNAME";
+	public handleInput inputHandler = new handleInput();
+	public determineOutput outputDeterminer = new determineOutput();
+	public Personality p = new Personality();
+	public questionAsker questionAsker = new questionAsker();
 	public boolean genderchosen = false;
 	public boolean turn = false;
 	public boolean nameknown = false;
-	public Personality p = new Personality("man");
+	public String username = "Human";
+	public String chatbotname = "CHATBOTNAME";
 	public String botoutput = "";
 	public String data = "";
 	public String qdata = "";
-	public String qresponse[] = new String[50];
-	public String question[] = new String[2];
-	public questionAsker questionAsker = new questionAsker();
-	String userinput = "";
+	public String qresponse[] = new String[2];
+	public String question[] = new String[2];	
+	public String userinput = "";
+	public boolean askName = false;
+	public String result;
 	public static void main(String[] args) {
-		// Initialize required attributes and objects
-		// Used for processing input
 		launch(args);
-	}		
+	}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Button button;
-		handleInput inputHandler = new handleInput();
-		// Used to determine output based on processed input
-		determineOutput outputDeterminer = new determineOutput();
-		
-		// Initialize booleans for determining steps in conversation
-		
-		// Initialize names for identifying speakers
-		
-		//System.out.println();
-		//*************************************************************************************
+		//Set Scene ***********************************************************
 		primaryStage.setTitle("Dating Bot");		
 		HBox hb = new HBox();
 		HBox hb2 = new HBox();
@@ -61,14 +57,14 @@ public class terminalwgui extends Application{
 	    outField.setPrefWidth(500);
 	    outField.setPrefHeight(75);
 	    
-	    TextField inField = new TextField ("");
-	    inField.setPrefWidth(200);
-	    inField.setPrefHeight(50);
+	    TextInputDialog inField = new TextInputDialog();	   
+	    inField.setWidth(200);
+	    inField.setHeight(50);
 	   
 	    hb.getChildren().add(outField);
 	    hb.setAlignment(Pos.TOP_CENTER);
 	    hb.setPadding(new Insets(100, 20, 20, 20));
-	    hb2.getChildren().addAll(inField, actionBtn);
+	    hb2.getChildren().add(actionBtn);
 	    hb2.setAlignment(Pos.BOTTOM_CENTER);
 	    hb2.setPadding(new Insets(20, 20, 20, 20));
 	    
@@ -79,23 +75,30 @@ public class terminalwgui extends Application{
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
-		layout.setStyle("-fx-background-image: url('https://wallpaperstock.net/wallpapers/thumbs1/34464wide.png');");
-		
-		userinput = inField.getText().toString();
-		
-		actionBtn.setOnAction(new EventHandler<ActionEvent>() {
-		    @Override public void handle(ActionEvent e) {
+		layout.setStyle("-fx-background-image: url('https://wallpaperstock.net/wallpapers/thumbs1/34464wide.png');");		
+		//***********************************************************************
+		actionBtn.setOnAction(new EventHandler<ActionEvent>() {			
+		    @Override public void handle(ActionEvent e) {		    	
+		    	//System.out.print("\n" + username + ":");
+				//String userinput = inputHandler.getUserInput();
 				// Loop is called after desired gender and name are chosen, and begins to loop
 				// through response/questions
+		    	result = inField.showAndWait().toString();
+		    	result = result.substring(9, result.length() - 1);		    	
 				if (genderchosen == true && nameknown == true) {
-					data = inputHandler.parseInput(userinput);
+					data = inputHandler.parseInput(result);
 					if (data.equals("nothing")) {
 						question = questionAsker.ask();
 						outField.setText(chatbotname + ": " + question[0]);
 						qdata = question[1];
-						userinput = inField.getText();
-						qresponse = inputHandler.parseQResponse(userinput, qdata, p);
-						botoutput = questionAsker.afterAsk(userinput, question[1], p);
+						result = inField.showAndWait().toString();	
+						result = result.substring(9, result.length() - 1);
+						qresponse = inputHandler.parseQResponse(result.toString(), qdata, p);
+						if (userinput.endsWith("?")) {
+							botoutput = outputDeterminer.respond(inputHandler.keywordConvert(qdata), p);
+							outField.setText(chatbotname + botoutput);
+						}
+						botoutput = questionAsker.afterAsk(qresponse, qdata);
 					} else {
 						botoutput = outputDeterminer.respond(data, p);
 
@@ -104,20 +107,20 @@ public class terminalwgui extends Application{
 				}
 				// Determine desired gender from user
 				if (genderchosen == false) {
-					String gender = inputHandler.checkGender(userinput);
+					String gender = inputHandler.checkGender(result);
 					p = new Personality(gender);
 					chatbotname = p.getName();
-					//System.out.println(chatbotname);
 					outField.setText("You are now on a date with a " + gender + " named " + chatbotname + ".");
 					genderchosen = true;
 				}
 				// Determining users name, occupation from user
-				if (nameknown == false) {
+				else if (nameknown == false) {
 					outField.setText(chatbotname + ": Hi! What's your name?");
 					//System.out.println(username + ":");
-					userinput = inField.getText();
-					username = inputHandler.parseName(userinput).substring(0, 1).toUpperCase()
-							+ inputHandler.parseName(userinput).substring(1);
+					result = inField.showAndWait().toString();
+					result = result.substring(9, result.length() - 1);
+					username = inputHandler.parseName(result).substring(0, 1).toUpperCase()
+							+ inputHandler.parseName(result).substring(1);
 					nameknown = true;
 					int random = (int) (Math.random() * 2 + 1);
 					if (random == 1)
@@ -125,16 +128,15 @@ public class terminalwgui extends Application{
 					else
 						outField.setText(chatbotname + ": That's a lovely name, " + username + ". So, what do you do for a living?");
 					//System.out.println(username + ":");
-					userinput = inField.getText();
-					outField.setText(chatbotname + ": " + outputDeterminer.occupation(inputHandler.checkOccupation(userinput)));
-
+					result = inField.showAndWait().toString();
+					result = result.substring(9, result.length() - 1);
+					outField.setText(chatbotname + ": " + outputDeterminer.occupation(inputHandler.checkOccupation(result)));
 				}
-			if (inputHandler.parseInput(userinput).equals("bye"))
-				System.exit(0);
-			turn = !turn;
-		    }
-		 });
-
-	}
+				// End conversation if user types "bye"
+				if (inputHandler.parseInput(userinput).equals("bye"))
+				
+				turn = !turn;
+		}
+	});		
 }
-
+}
